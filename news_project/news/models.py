fcda98182d86f2b0577354f151128cadd4ae4822
OttoGotlieb <antagonist1313@gmail.com>
2021-04-09 22:28:50 +0300

    main page added

--- news_project/news/models.py
+++ news_project/news/models.py
@@ -7,6 +7,21 @@ class News(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name_plural = 'News'
+        ordering = ['-created_at', 'title']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Categories'
--- news_project/news/views.py
+++ news_project/news/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import TemplateView, ListView
-from .models import News
+from django.views.generic import ListView
+from .models import News, Category
 
 
 class Index(ListView):
@@ -8,9 +8,13 @@ class Index(ListView):
     news = News.objects.all()
 
     def get_queryset(self):
-        return News.objects.order_by('-created_at')
+        return News.objects.all()
+
+    def get_categories(self):
+        return Category.objects.all()
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['News'] = self.get_queryset()
+        context['Categories'] = self.get_categories()
         return context
